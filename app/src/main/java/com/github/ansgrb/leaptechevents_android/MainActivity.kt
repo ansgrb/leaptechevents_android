ba2c72df4658437011baf4bb44fca3668e50e915
ansgrb <ansgrb@gmail.com>
2025-06-11 11:06:53 +0300

    refactor: Update dependencies and remove unused code
    - Update versions for Kotlin, Compose BOM, Hilt, Firebase services, Coil, Mockito, and other libraries in `libs.versions.toml`.
    - Update plugin versions in `build.gradle.kts` (project level).
    - Apply new plugins (google-ksp, kotlin-serialization, google-services, firebase-crashlytics, google-hilt) and remove deprecated KSP classpath in `app/build.gradle.kts`.
    - Add dependencies for AndroidX Core Splashscreen, Navigation Compose, and Hilt Navigation Compose in `app/build.gradle.kts`.
    - Remove unused `Greeting` Composable and its preview from `MainActivity.kt`.
    - Comment out NavController initialization in `MainActivity.kt`.

--- app/src/main/java/com/github/ansgrb/leaptechevents_android/MainActivity.kt
+++ app/src/main/java/com/github/ansgrb/leaptechevents_android/MainActivity.kt
@@ -4,13 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.github.ansgrb.leaptechevents_android.ui.theme.LeapTechEventsAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +12,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LeapTechEventsAndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+//                val navController = rememberNavController()
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LeapTechEventsAndroidTheme {
-        Greeting("Android")
     }
 }
