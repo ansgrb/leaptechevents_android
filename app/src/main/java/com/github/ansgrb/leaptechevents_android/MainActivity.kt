be110c9540216faf231458efacf614925e036ba8
ansgrb <ansgrb@gmail.com>
2025-06-11 11:22:31 +0300

    feat: Implement basic navigation graph
    - Add NavHost with placeholder composable routes for:
        - login
        - event_list
        - event_detail/{eventId}
        - booking/{eventId}
        - e_ticket/{ticketId}
        - ticket_history
        - qr_scanner
    - Utilize `rememberNavController` to manage navigation state.

--- app/src/main/java/com/github/ansgrb/leaptechevents_android/MainActivity.kt
+++ app/src/main/java/com/github/ansgrb/leaptechevents_android/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.github.ansgrb.leaptechevents_android.ui.theme.LeapTechEventsAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,8 +15,30 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LeapTechEventsAndroidTheme {
-//                val navController = rememberNavController()
-
+                val navController = rememberNavController()
+                NavHost(navController, startDestination = "login") {
+                    composable("login") {
+                        // TODO: Handle login logic
+                    }
+                    composable("event_list") {
+                        // TODO: Handle event list logic
+                    }
+                    composable("event_detail/{eventId}") { backStackEntry ->
+                        // TODO : Handle event detail logic
+                    }
+                    composable("booking/{eventId}") { backStackEntry ->
+                        // TODO: Handle booking logic
+                    }
+                    composable("e_ticket/{ticketId}") { backStackEntry ->
+                        // TODO: Handle e-ticket logic
+                    }
+                    composable("ticket_history") {
+                        // TODO: Handle ticket history logic
+                    }
+                    composable("qr_scanner") {
+                        // TODO: Handle QR scanner logic
+                    }
+                }
             }
         }
     }
