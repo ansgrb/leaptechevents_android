264e0a6fbe3dda43d842b65d705e3dcb1f50984a
ansgrb <ansgrb@gmail.com>
2025-06-13 13:08:16 +0300

    feat: Enhance Splash Screen with Gradient Button and Navigation
    - Add a "Get Started" `GradientButton` to `SplashScreen.kt`.
    - Implement navigation from `SplashActivity` to `MainActivity` on button click.
    - Make `SplashActivity` the launcher activity in `AndroidManifest.xml`.
    - Update theme `Theme.LeapTechEventsAndroid` to inherit from `Theme.AppCompat.Light.NoActionBar`.
    - Remove `app/build.gradle` and `.env.example` as they are no longer needed.
    - Update IntelliJ IDEA project files (`.idea/workspace.xml`, `.idea/deploymentTargetSelector.xml`).
    - Remove unused module file `.idea/modules/app/Leap_Tech_Events_-_Android.app.iml`.

--- app/src/main/java/com/github/ansgrb/leaptechevents_android/presentation/components/GradientButton.kt
+++ app/src/main/java/com/github/ansgrb/leaptechevents_android/presentation/components/GradientButton.kt
@@ -16,16 +16,25 @@
  */
 package com.github.ansgrb.leaptechevents_android.presentation.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.github.ansgrb.leaptechevents_android.R
 
 @Composable
 @Preview
@@ -41,5 +50,28 @@ fun GradientButton(
         modifier = Modifier
             .fillMaxWidth()
             .padding(padding.dp)
-    ) { }
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 12.dp)
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = listOf(
+                            colorResource(R.color.light_purple),
+                            colorResource(R.color.pink)
+                        )
+                    ),
+                    shape = RoundedCornerShape(50.dp)
+                )
+        ) {
+            Text(
+                text = text,
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp
+            )
+        }
+    }
 }
